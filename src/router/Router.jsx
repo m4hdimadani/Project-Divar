454509cf7eda0458cc2a1987bf7a34d74a321ff0
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-02-25 09:54:34 +0330

    set  access token in headers

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,21 +1,26 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "src/services/user";
 
-import PageNotFound from 'pages/404'
-import AdminPage from 'pages/AdminPage'
-import AuthPage from 'pages/AuthPage'
-import DashboardPage from 'pages/DashboardPage'
-import HomePage from 'pages/HomePage'
+import PageNotFound from "pages/404";
+import AdminPage from "pages/AdminPage";
+import AuthPage from "pages/AuthPage";
+import DashboardPage from "pages/DashboardPage";
+import HomePage from "pages/HomePage";
 
 function Router() {
+  const { data, isLoading } = useQuery(["profile"], getProfile);
+
+  if (isLoading) return <h1>Loading ... </h1>;
   return (
     <Routes>
-    <Route index element={<HomePage />} />
-    <Route path="/dashboard" element={<DashboardPage />} />
-    <Route path="/auth" element={<AuthPage />} />
-    <Route path="/admin" element={<AdminPage />} />
-    <Route path="*" element={<PageNotFound />} />
-  </Routes>
-  )
+      <Route index element={<HomePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
 
-export default Router
+export default Router;
---
+++ src/services/user.js
@@ -0,0 +1,8 @@
+import api from "src/configs/api";
+
+
+
+const getProfile = () =>
+  api.get("user/whoami");
+
+export { getProfile };
