0f47dbfb9d04d7e88d53fa6be1da0d0b47381ba3
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-03-05 08:27:07 +0330

    send data category to Backend

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,25 +1,40 @@
 import { useState } from "react";
-import styles from "./CategoryForm.module.css"
+import styles from "./CategoryForm.module.css";
+import { useMutation } from "@tanstack/react-query";
+import { addCategory } from "src/services/admin";
 
 function CategoryForm() {
   const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+
+  const { mutate, isLoading, error, data } = useMutation(addCategory);
+
   const changeHandler = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+    if (!form.name || !form.slug || !form.icon) return;
+    mutate(form);
   };
   return (
-    <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}>
+    <form
+      onChange={changeHandler}
+      onSubmit={submitHandler}
+      className={styles.form}
+    >
       <h3>دسته بندی جدید</h3>
+      {!!error && <p> مشکلی پیش آمده است</p>}
+      {data?.status === 201 && <p> دسته بندی با موفقیت اضافه شد شد</p>}
       <label htmlFor="name">اسم دسته بندی</label>
       <input type="text" name="name" id="name" />
       <label htmlFor="slug"> اسلاگ</label>
       <input type="text" name="slug" id="slug" />
       <label htmlFor="icon"> آیکون</label>
       <input type="text" name="icon" id="icon" />
-      <button type="submit">ایجاد</button>
+      <button type="submit" disabled={isLoading}>
+        ایجاد
+      </button>
     </form>
   );
 }
